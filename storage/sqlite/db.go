8dfef3bf43e4b5e9d06726c01caa58dc7e97c963
Leonardo Ribeiro <leonardovictor@outlook.com>
2025-05-12 14:13:05 -0300

    update db and add migrate operation

--- storage/sqlite/db.go
+++ storage/sqlite/db.go
@@ -3,6 +3,9 @@ package storage
 import (
 	"database/sql"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	"github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -21,4 +24,30 @@ func New(dsn string) (*DB, error) {
 	}
 
 	return &DB{db}, nil
+}
+
+func (db *DB) Migrate() error {
+	instance, err := sqlite3.WithInstance(db.DB, &sqlite3.Config{})
+	if err != nil {
+		return err
+	}
+
+	fs, err := (&file.File{}).Open("./storage/migrations")
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithInstance("file", fs, "sqlite3", instance)
+	if err != nil {
+		return err
+	}
+
+	if err = m.Down(); err != nil {
+		return err
+	}
+
+	if err = m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+	return nil
 }
